946f7fa200c28b0f56dfdae620fb939c1aa67be4
thalison campos monteiro <thalison.m@lyncas.net>
2022-04-20 16:09:00 -0300

    Commit inicial

---
+++ src/main/java/net/lyncas/scienceTools/scienceTools/ScienceToolsApplication.java
@@ -0,0 +1,13 @@
+package net.lyncas.scienceTools.scienceTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScienceToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScienceToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/lyncas/scienceTools/scienceTools/ScienceToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lyncas.scienceTools.scienceTools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScienceToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
